0eeef507e3cf4b275f32ae3a68ef29c6f9751297
Ilya Kryazh <ilya.kryazik10@gmail.com>
2020-05-25 20:32:28 +0300

    Add function "Start Game"

--- LifeGame/Form1.cs
+++ LifeGame/Form1.cs
@@ -12,9 +12,48 @@ namespace LifeGame
 {
     public partial class Form1 : Form
     {
+        private Graphics graphics;
+        private int resolution;
+        private bool[,] field;
+        private int rows;
+        private int cols;
+
         public Form1()
         {
             InitializeComponent();
+        }
+      
+        // Function that fill Array filed, turn of buutoms and inputs while timer turn on 
+        public void StartGame()
+        {
+            if (timer1.Enabled)
+            {
+                return;
+            }
+
+            nudResolution.Enabled = false;
+            nudDensity.Enabled = false;
+
+            resolution = (int)nudResolution.Value;
+
+            // game fields array
+            rows = pictureBox1.Height / resolution;
+            cols = pictureBox1.Width / resolution;
+            field = new bool[cols, rows];
+
+            Random random = new Random();
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = random.Next((int)nudDensity.Value) == 0;
+                }
+            }
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+
+            timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -24,7 +63,7 @@ namespace LifeGame
 
         private void start_Click(object sender, EventArgs e)
         {
-
+            StartGame();
         }
 
         private void stop_Click(object sender, EventArgs e)
